43c87bd59ff804b6a4bd953279bca47efffe711f
Rense Bakker <b.rense@gmail.com>
2022-04-28 09:55:45 +0200

    improved rooms list

--- src/components/CreateOrJoinRoom.tsx
+++ src/components/CreateOrJoinRoom.tsx
@@ -1,11 +1,11 @@
 import { Button, Card, Box, List, ListItem, ListItemText, ListItemSecondaryAction } from '@mui/material'
 
 function CreateOrJoinRoom({ broadcasts, onJoin, onCall, isConnected }: { isConnected: boolean, broadcasts: { [key: string]: string }, onJoin: (room: string) => void, onCall: (payload: { isBroadcast: boolean }) => void }) {
-  return <Box>
-    <Card>
+  return <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+    <Card sx={{ marginBottom: 2 }}>
       <List disablePadding>
         {Object.keys(broadcasts).map(k => <ListItem key={k}>
-          <ListItemText primary={k} />
+          <ListItemText primary="Room ID" secondary={k} secondaryTypographyProps={{ component: 'code', sx: { fontFamily: '\'Roboto Mono\', monospace', fontWeight: 200 } }} />
           <ListItemSecondaryAction>
             <Button size="small" onClick={() => onJoin(k)}>Join</Button>
           </ListItemSecondaryAction>
