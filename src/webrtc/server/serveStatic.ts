98f98f018262010fb02e4b16fc8d2642b4a3989e
Rense Bakker <b.rense@gmail.com>
2022-05-02 16:13:12 +0200

    fixed serve static files

--- src/webrtc/server/serveStatic.ts
+++ src/webrtc/server/serveStatic.ts
@@ -3,21 +3,21 @@ import path from 'path'
 import fs from 'fs'
 
 function serveStatic(app: Express) {
-  app.use('/static', express.static(path.resolve(__dirname, 'static')))
+  app.use('../static', express.static(path.resolve(__dirname, 'static')))
   app.use(express.json())
   app.use(express.urlencoded({ extended: false }))
   app.get(/^(?!\/socket.io).*$/, serveStaticFiles)
 
   function serveStaticFiles(req: Request, res: Response) {
     if (path.extname(req.path) !== '') {
-      const resolvedPath = path.resolve(__dirname, `.${req.path}`)
+      const resolvedPath = path.resolve(__dirname, `..${req.path}`)
       if (fs.existsSync(resolvedPath)) {
         res.sendFile(resolvedPath)
       } else {
         res.sendStatus(404)
       }
     } else {
-      const index = path.resolve(__dirname, 'index.html')
+      const index = path.resolve(__dirname, '../index.html')
       res.sendFile(index)
     }
   }
